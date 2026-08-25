5d0035fa94e64f9a70cda190016809b8dad4208a
Soumyakanta Bera <155274196+soumyakantabera@users.noreply.github.com>
2026-08-25 09:51:12 +0200

    Remove skip to main content link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,9 +7,6 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <OrgJsonLd />
-      <a href="#main-content" className="skip-link">
-        Skip to main content
-      </a>
       <Header />
       <main id="main-content">{children}</main>
       <Footer />
